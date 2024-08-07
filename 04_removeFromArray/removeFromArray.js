ae2e8ce218c92dc067a549bfbd928bd72d4f17ff
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-07 19:25:45 +1000

    first 2 tests of exercise 4 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, rm1, rm2) {
+if (typeof rm2 === 'undefined' ) {
+    array.splice(array.indexOf(rm1),1);
+    return array;
+} else if (typeof rm2 !== 'undefined') {
+    array.splice(array.indexOf(rm1),1);
+    array.splice(array.indexOf(rm2),1);
+    return array;
+}
+}
 // Do not edit below this line
 module.exports = removeFromArray;
