32b616c41524e1566406de7d11a74d3893c75e03
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-07 14:44:58 +1000

    Completed exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word,n) {
+let newString = ''; 
+if (n < 0) {
+    return 'ERROR';
+} else {
+    for (let i = 0; i < n; i++) {
+        newString += word;
+    }
+  return newString;
 };
-
+}
 // Do not edit below this line
 module.exports = repeatString;
