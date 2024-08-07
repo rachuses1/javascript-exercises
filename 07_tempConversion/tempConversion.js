1c4a9ae3f24c4a007d7fb95f813e89999d80599b
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-07 18:01:05 +1000

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5/9);
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+  return Number(result.toFixed(1));
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9/5)) + 32;
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+  return Number(result.toFixed(1));
 };
+}
 
 // Do not edit below this line
 module.exports = {
