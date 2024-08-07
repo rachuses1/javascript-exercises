e77868fffe2e6a0d2ba57cc7fc3682c53f2e396d
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-07 19:55:26 +1000

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,32 @@
-const removeFromArray = function(array, rm1, rm2) {
-if (typeof rm2 === 'undefined' ) {
-    array.splice(array.indexOf(rm1),1);
-    return array;
+const removeFromArray = function(array, rm1, rm2, rm3, rm4) {
+
+    function checkNo(value) {
+        return value !== rm1;
+    }
+
+    function checkNo2(value) {
+        return value !== rm2;
+    }
+
+    function checkNo3(value) {
+        return value !== rm3;
+    }
+
+    function checkNo4(value) {
+        return value !== rm4;
+    }
+
+if (typeof rm2 === 'undefined' || rm2 === 'string') {
+    const result = array.filter(checkNo);
+    return result;
+
+} else if (typeof rm4 !=='undefined') {
+    const result = array.filter(checkNo).filter(checkNo2).filter(checkNo3).filter(checkNo4);
+    return result;
+    
 } else if (typeof rm2 !== 'undefined') {
-    array.splice(array.indexOf(rm1),1);
-    array.splice(array.indexOf(rm2),1);
-    return array;
+    const result = array.filter(checkNo).filter(checkNo2);
+    return result;
 }
 }
 // Do not edit below this line
