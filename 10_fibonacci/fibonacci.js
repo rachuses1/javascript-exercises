9e9f43b06d0c3cb5593943b9c971a31b6e97aae9
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-11 18:54:20 +1000

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    
+    if (typeof n === 'string') {
+        n = Number(n);
+    } if (n === 0) {
+        return Number(n)
+    } else if (n < 0) {
+        return 'OOPS'
+    }
+
+    let sequence = [1, 1];
+
+    for(let i = 1; i < n; i++) {
+        let addition = sequence[i] + sequence[i - 1];
+        sequence.push(addition);
+    }
+    return sequence[n - 1];
 
 };
 
