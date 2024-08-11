685a06548d79513cb5d5b5f68a1da073f074b5f6
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-11 17:45:43 +1000

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
 
-};
+let convert = '';
+
+convert = phrase.replaceAll('!','')
+                .replaceAll('.','')
+                .replaceAll(',','')
+                .replaceAll(' ','')
+                .toLowerCase();
+
+let reverse = ''; 
+for (let n = convert.length-1; n >= 0; n--) {
+    reverse += convert[n]
+    } // reverse
+
+return convert === reverse;
+} 
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
