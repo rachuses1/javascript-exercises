4109eabf7c87e98673a18e9955b083dbf56ba065
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-11 17:11:43 +1000

    completed exercises 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumAll = array.reduce((total, num) => total + num, 0)
+  return sumAll; 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiply = array.reduce((total, num) => total * num, 1);
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let powerOf = a ** b;
+  return powerOf;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+	let array = [];
+  
+  if(a === 0) {
+    array.push(1);
+  } else if (a > 0) { 
+    for(let i = 0; i < a; i++) 
+      {array.push(a - i);}
+  }
+
+  let result = array.reduce((total, num) => total * num, 1);
+  return result;
+}
+
 
 // Do not edit below this line
 module.exports = {
