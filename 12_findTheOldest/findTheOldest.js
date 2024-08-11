b617ce39cca8d793e4e69930db98e7e8180b95d3
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-11 20:18:18 +1000

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+
+
+    const ages = people.map((person) => {
+        if(!person.yearOfDeath) {
+            Object.defineProperty(person,'yearOfDeath', {value: 2024});
+        };
+        return (person.yearOfDeath - person.yearOfBirth);
+    });
+    
+    const oldest = Math.max(...ages);
+
+    const number = ages.findIndex((age) => age === oldest);
+
+    return people[number];
+
+}
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
