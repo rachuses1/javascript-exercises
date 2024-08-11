5814aee058d29661c65276f76bbe8e19b9e7451c
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-11 19:19:02 +1000

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    // let revised = [];
+
+    // for(let i = 0; i < books.length; i++) {
+    //     revised.push(books[i].title)
+    // }
+
+    return books.map((book) => book.title)
+
+// return revised
 };
 
 // Do not edit below this line
